61bc42d6a90a7373f0e35258e4878a6e5e75f322
slofurno <slofurno@gmail.com>
2016-03-15 10:37:54 -0400

    tevs

--- code/fetch.go
+++ code/fetch.go
@@ -1,10 +1,25 @@
 func (s *Client) Fetch(key string) <-chan *Tuple {
-	idx := crc16(key) % 4
-	sock.Dial(servers[idx])
-	sock.SetOption(mangos.OptionSubscribe, key)
-	
-	go func() {
-		http.Get(uri + key)
-	
+	sock, _ := sub.NewSocket()
 
+	si := Crc16(key) % 4
+	uri := servers[i]
+
+	sock.SetOption(mangos.OptionSubscribe, key)
+	items := make(chan *Tuple, 64)
+
+	go func() {
+		res, _ := http.Get(uri + key)
+		json.Unmarshal(b, &xs)
+		for i := 0; i < len(xs); i++ {
+			items <- xs[i]
+		}
+
+		for {
+			msg, err := sock.Recv()
+			json.Unmarshal(body, x)
+			items <- x
+		}
+	}()
+
+	return items
 }
